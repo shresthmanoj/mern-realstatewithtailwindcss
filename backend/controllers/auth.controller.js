29d21d8f658539428307bdf3a11dd3e4b8717b31
shresthmanoj <mz4sj@outlook.com>
2023-09-27 19:38:50 +0545

    create signup route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,16 @@
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res) => {
   console.log(req.body);
   const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
 
-  await newUser.save();
-
-  res.status(201).json("User created sucessfully!");
+  try {
+    await newUser.save();
+    res.status(201).json("User created sucessfully!");
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 };
