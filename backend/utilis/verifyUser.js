b262e955c4f7b72d79812312c6d43506fde86b0d
shresthmanoj <mz4sj@outlook.com>
2023-09-30 22:28:01 +0545

    user update api route

---
+++ backend/utilis/verifyUser.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+import { errorHandler } from "./error.js";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+  if (!token) return next(errorHandler(401, "you are unauthorized"));
+  //user is user_id
+  jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
+    if (error) return next(errorHandler(403, "Forbidden"));
+    req.user = user;
+    next();
+  });
+};
