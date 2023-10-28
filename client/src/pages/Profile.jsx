2f8802dd2e28d0959cba514df0a43dc079fa6100
shresthmanoj <mz4sj@outlook.com>
2023-10-28 14:24:04 +0545

    update edit listing function

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -158,6 +158,24 @@ export default function Profile() {
       setShowListingError(true);
     }
   };
+
+  const handleDeleteListing = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setUserListing((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -272,13 +290,16 @@ export default function Profile() {
                 <p>{listing.name}</p>
               </Link>
               <div className="flex flex-col items-center">
-                <button className="text-red-700 uppercase">Delete</button>
-                <button className="text-green-700 uppercase">Edit</button>
+                <button
+                  onClick={() => handleDeleteListing(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className="text-green-700 uppercase">Edit</button>
+                </Link>
               </div>
-
-              <p>{listing.description}</p>
-              <p>{listing.price}</p>
-              <p>{listing.address}</p>
             </div>
           ))}
         </div>
