7227cb067662f87549a225057ca0fb19b66f2759
shresthmanoj <mz4sj@outlook.com>
2023-10-28 10:29:58 +0545

    show user listing

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import { errorHandler } from "../utilis/error.js";
 import bcryptjs from "bcryptjs";
+import Listing from "./../models/listing.model.js";
 
 export const test = (req, res) => {
   res.json({
@@ -47,4 +48,16 @@ export const deleteUser = async (req, res, next) => {
     next(error);
   }
 };
- 
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can view only your own litings!"));
+  }
+};
